95d6fd647291e790fbe4ecd2ff816396bae03c3f
nikhil <nikhil.f4u80@gmail.com>
2023-05-16 20:48:10 +0530

    latest

--- src/app/components/option-chain-analysis/option-chain-analysis.component.ts
+++ src/app/components/option-chain-analysis/option-chain-analysis.component.ts
@@ -76,24 +76,26 @@ export class OptionChainAnalysisComponent implements OnInit {
   public lineChartForPutPriceAndOI: Partial<LinesChartOptions> | any;
   public callPutOITrendLineChart: Partial<LineChartOptions> | any;
   strikePriceList: number[] = [];
-  niftyStrikePriceList:number[]=[];
-  bankNiftyStrikePriceList:number[]=[];
+  niftyStrikePriceList: number[] = [];
+  bankNiftyStrikePriceList: number[] = [];
   selectedIndex: string = "nifty";
   isChangeInOIEnabled: boolean = true;
   isCallPutTrendEnabled: boolean = false;
-  selectedStrikePrice: number=0;
-  niftyOpeningPrice:number=18250;
-  bankNiftyOpeningPrice:number=42300;
-  daysLeftToExpiry:number=4;
-  constructor(private service: GraphqlService, private snackBar: MatSnackBar,public dialog: MatDialog) {
+  selectedStrikePrice: number = 0;
+  niftyOpeningPrice: number = 18400;
+  bankNiftyOpeningPrice: number = 44100;
+  daysLeftToExpiry: number = 3;
+  niftyPCR:string="";
+  bnfPCR:string="";
+  constructor(private service: GraphqlService, private snackBar: MatSnackBar, public dialog: MatDialog) {
     this.getNiftyDataAndPlotChart();
     this.getOptionChainDataAndSaveToBackend();
   }
 
   ngOnInit(): void {
-    setInterval(()=>{
+    setInterval(() => {
       this.getOptionChainDataAndSaveToBackend();
-    },60000) //
+    }, 60000) //
     setInterval(() => {
       if (this.selectedIndex == 'nifty') {
         this.getNiftyDataAndPlotChart();
@@ -101,70 +103,74 @@ export class OptionChainAnalysisComponent implements OnInit {
       else {
         this.getNiftyBankDataAndPlotChart();
       }
-    }, 60000) 
+    }, 60000)
 
   }
-  getOptionChainDataAndSaveToBackend()
- {
-  this.service.getNiftyOptionChainData().subscribe((data) => {
-           this.prepareOptionChainDtoListAndSave(data,'nifty');
-  });
-  this.service.getNiftyBankOptionChainData().subscribe((data)=>{
-    this.prepareOptionChainDtoListAndSave(data,'niftybank');
-  })
- }
- 
- prepareOptionChainDtoListAndSave(data:any,indexType:string)
- {
-  console.log(data);
-  let time = moment().format("HH:mm");
-  let range = 500;
-  let roundValue = 50;
-  let lotSize = 50;
-  let openingPrice=this.niftyOpeningPrice;
-  if (indexType == 'niftybank') {
-     range = 1000;
-     roundValue = 100;
-     lotSize = 25;
-     openingPrice=this.bankNiftyOpeningPrice
+  getOptionChainDataAndSaveToBackend() {
+    this.service.getNiftyOptionChainData().subscribe((data) => {
+      this.niftyPCR=(Math.round((data.filtered.PE.totOI/data.filtered.CE.totOI)*100)/100).toFixed(2);
+      console.log(this.niftyPCR);
+      this.prepareOptionChainDtoListAndSave(data, 'nifty');
+    });
+    this.service.getNiftyBankOptionChainData().subscribe((data) => {
+      this.bnfPCR=(Math.round((data.filtered.PE.totOI/data.filtered.CE.totOI)*100)/100).toFixed(2);
+      console.log(this.bnfPCR)
+      this.prepareOptionChainDtoListAndSave(data, 'niftybank');
+    })
   }
-  let optionChainData: OptionChainInputDto[] = [];
-  
-  for (let index = 0; index < data.filtered.data.length; index++) {
-    if (Math.abs(data.filtered.data[index].strikePrice - openingPrice) <= range) {
-      let ceOIDAta: OptionChainInputDto = new OptionChainInputDto();
-      ceOIDAta.openInterest = data.filtered.data[index].CE.changeinOpenInterest * lotSize;
-      ceOIDAta.price = data.filtered.data[index].CE.lastPrice;
-      ceOIDAta.strikePrice = data.filtered.data[index].CE.strikePrice + "CE";
-      ceOIDAta.impliedVolatility=data.filtered.data[index].CE.impliedVolatility;
-      ceOIDAta.time = time;
-      ceOIDAta.index=indexType;
-      ceOIDAta.underlyingValue=parseInt(data.filtered.data[index].CE.underlyingValue);
-      ceOIDAta.daysLeftToExpire=this.daysLeftToExpiry;
-      ceOIDAta.totalTradedVolume=data.filtered.data[index].CE.totalTradedVolume;
-      optionChainData.push(ceOIDAta);
-      let peOIDAta: OptionChainInputDto = new OptionChainInputDto();
-      peOIDAta.openInterest = data.filtered.data[index].PE.changeinOpenInterest * lotSize;
-      peOIDAta.price = data.filtered.data[index].PE.lastPrice;
-      peOIDAta.impliedVolatility=data.filtered.data[index].PE.impliedVolatility;
-      peOIDAta.strikePrice = data.filtered.data[index].PE.strikePrice + "PE";
-      peOIDAta.time = time;
-      peOIDAta.index=indexType;
-      peOIDAta.underlyingValue=parseInt(data.filtered.data[index].PE.underlyingValue);
-      peOIDAta.daysLeftToExpire=this.daysLeftToExpiry;
-      peOIDAta.totalTradedVolume=data.filtered.data[index].PE.totalTradedVolume;
-      optionChainData.push(peOIDAta);
+
+  prepareOptionChainDtoListAndSave(data: any, indexType: string) {
+    console.log(data);
+    let time = moment().format("HH:mm");
+    let range = 500;
+    let roundValue = 50;
+    let lotSize = 50;
+    let openingPrice = this.niftyOpeningPrice;
+    if (indexType == 'niftybank') {
+      range = 1000;
+      roundValue = 100;
+      lotSize = 25;
+      openingPrice = this.bankNiftyOpeningPrice
     }
+    let optionChainData: OptionChainInputDto[] = [];
+
+    for (let index = 0; index < data.filtered.data.length; index++) {
+      if (Math.abs(data.filtered.data[index].strikePrice - openingPrice) <= range) {
+        let ceOIDAta: OptionChainInputDto = new OptionChainInputDto();
+        ceOIDAta.openInterest = data.filtered.data[index].CE.changeinOpenInterest * lotSize;
+        ceOIDAta.price = data.filtered.data[index].CE.lastPrice;
+        ceOIDAta.strikePrice = data.filtered.data[index].CE.strikePrice + "CE";
+        ceOIDAta.impliedVolatility = data.filtered.data[index].CE.impliedVolatility;
+        ceOIDAta.time = time;
+        ceOIDAta.index = indexType;
+        ceOIDAta.underlyingValue = parseInt(data.filtered.data[index].CE.underlyingValue);
+        ceOIDAta.daysLeftToExpire = this.daysLeftToExpiry;
+        ceOIDAta.totalTradedVolume = data.filtered.data[index].CE.totalTradedVolume;
+        optionChainData.push(ceOIDAta);
+        let peOIDAta: OptionChainInputDto = new OptionChainInputDto();
+        peOIDAta.openInterest = data.filtered.data[index].PE.changeinOpenInterest * lotSize;
+        peOIDAta.price = data.filtered.data[index].PE.lastPrice;
+        peOIDAta.impliedVolatility = data.filtered.data[index].PE.impliedVolatility;
+        peOIDAta.strikePrice = data.filtered.data[index].PE.strikePrice + "PE";
+        peOIDAta.time = time;
+        peOIDAta.index = indexType;
+        peOIDAta.underlyingValue = parseInt(data.filtered.data[index].PE.underlyingValue);
+        peOIDAta.daysLeftToExpire = this.daysLeftToExpiry;
+        peOIDAta.totalTradedVolume = data.filtered.data[index].PE.totalTradedVolume;
+        optionChainData.push(peOIDAta);
+      }
+    }
+    this.service.saveOIData(optionChainData).subscribe((response) => {
+      console.log(response);
+    }, (err) => {
+      this.snackBar.open("Some Error Occured while saving data to bacend", "X");
+      setTimeout(() => { this.snackBar.dismiss() }, 10000); console.error(err)
+    })
   }
-  this.service.saveOIData(optionChainData).subscribe((response)=>{
-    console.log(response);
-  },(err)=>{this.snackBar.open("Some Error Occured while saving data to bacend", "X");
-  setTimeout(() => { this.snackBar.dismiss() }, 10000);console.error(err)})
- }
 
   getNiftyDataAndPlotChart() {
     this.service.getNiftyOptionChainData().subscribe((data) => {
-      this.getStrikePriceForARange(data,"nifty");
+      this.getStrikePriceForARange(data, "nifty");
     }, (error) => {
       this.snackBar.open("Some Error Occured", "X");
       setTimeout(() => { this.snackBar.dismiss() }, 10000)
@@ -173,13 +179,13 @@ export class OptionChainAnalysisComponent implements OnInit {
 
   getNiftyBankDataAndPlotChart() {
     this.service.getNiftyBankOptionChainData().subscribe((data) => {
-      this.getStrikePriceForARange(data,"niftybank");
+      this.getStrikePriceForARange(data, "niftybank");
     }, (error) => {
       this.snackBar.open("Some Error Occured", "X"); setTimeout(() => { this.snackBar.dismiss() }, 10000)
     })
   }
-  getStrikePriceForARange(data: any,indexType:string) {
-   
+  getStrikePriceForARange(data: any, indexType: string) {
+
     let strikePrice = [];
     let callOIChange = [];
     let putOIChange = [];
@@ -188,22 +194,20 @@ export class OptionChainAnalysisComponent implements OnInit {
     let range = 500;
     let roundValue = 50;
     let lotSize = 50;
-    let openingPrice=this.niftyOpeningPrice;
+    let openingPrice = this.niftyOpeningPrice;
     if (this.selectedIndex == 'niftybank') {
       range = 1000;
       roundValue = 100;
       lotSize = 25;
-      openingPrice=this.bankNiftyOpeningPrice;
+      openingPrice = this.bankNiftyOpeningPrice;
     }
-    let insertStrikePrice=true;
-  if(indexType=='nifty' && this.niftyStrikePriceList.length!=0)
-  {
-    insertStrikePrice=false;
-  }
-  if(indexType=='niftybank' && this.bankNiftyStrikePriceList.length!=0)
-  {
-    insertStrikePrice=false;
-  }
+    let insertStrikePrice = true;
+    if (indexType == 'nifty' && this.niftyStrikePriceList.length != 0) {
+      insertStrikePrice = false;
+    }
+    if (indexType == 'niftybank' && this.bankNiftyStrikePriceList.length != 0) {
+      insertStrikePrice = false;
+    }
     for (let index = 0; index < data.filtered.data.length; index++) {
       if (Math.abs(data.filtered.data[index].strikePrice - openingPrice) <= range) {
         strikePrice.push(data.filtered.data[index].strikePrice);
@@ -215,22 +219,21 @@ export class OptionChainAnalysisComponent implements OnInit {
           callOIChange.push(data.filtered.data[index].CE.openInterest * lotSize);
           putOIChange.push(data.filtered.data[index].PE.openInterest * lotSize)
         }
-          callVolume.push(data.filtered.data[index].CE.totalTradedVolume)
-          putVolume.push(data.filtered.data[index].PE.totalTradedVolume)
+        callVolume.push(data.filtered.data[index].CE.totalTradedVolume)
+        putVolume.push(data.filtered.data[index].PE.totalTradedVolume)
+        if (indexType == 'nifty' && insertStrikePrice) {
+          this.niftyStrikePriceList.push(data.filtered.data[index].CE.strikePrice);
+        }
+        else if (indexType == 'niftybank' && insertStrikePrice) {
+          this.bankNiftyStrikePriceList.push(data.filtered.data[index].CE.strikePrice);
+        }
       }
-      if(indexType=='nifty' && insertStrikePrice)
-      {
-        this.niftyStrikePriceList.push(data.filtered.data[index].CE.strikePrice);
-      }
-      else if(indexType=='niftybank' && insertStrikePrice)
-      {
-        this.bankNiftyStrikePriceList.push(data.filtered.data[index].CE.strikePrice);
-      }
+
     };
-    this.plotNiftyOiAndVolume(callOIChange,putOIChange,callVolume,putVolume,strikePrice);
+    this.plotNiftyOiAndVolume(callOIChange, putOIChange, callVolume, putVolume, strikePrice);
   }
 
-  plotNiftyOiAndVolume(callOIChange:number[],putOIChange:number[],callVolume:number[],putVolume:number[],strikePrice:number[]) {
+  plotNiftyOiAndVolume(callOIChange: number[], putOIChange: number[], callVolume: number[], putVolume: number[], strikePrice: number[]) {
     this.overAllOIChart = {
       series: [
         {
@@ -310,11 +313,11 @@ export class OptionChainAnalysisComponent implements OnInit {
   }
   changeIndexData() {
     if (this.selectedIndex == 'nifty') {
-      this.strikePriceList=this.niftyStrikePriceList;
+      this.strikePriceList = this.niftyStrikePriceList;
       this.getNiftyDataAndPlotChart();
     }
     else {
-      this.strikePriceList=this.bankNiftyStrikePriceList;
+      this.strikePriceList = this.bankNiftyStrikePriceList;
       this.getNiftyBankDataAndPlotChart()
     }
   }
@@ -413,7 +416,7 @@ export class OptionChainAnalysisComponent implements OnInit {
         ],
         chart: {
           height: 350,
-          type: "line"
+          type: "line",
         },
         stroke: {
           width: [3, 3]
@@ -427,7 +430,8 @@ export class OptionChainAnalysisComponent implements OnInit {
         // },
         labels: response.data.getOptionChainDataForStrikePrice.callTimeList,
         xaxis: {
-          type: "Time"
+          type: "Time",
+
         },
         yaxis: [
           {
@@ -488,62 +492,62 @@ export class OptionChainAnalysisComponent implements OnInit {
           }
         ]
       };
-      this.strikePriceOpenInterestChangeLineChart = {
-        series: [
-          {
-            name: "OI Change",
-            data: response.data.getOptionChainDataForStrikePrice.overallChangeInOIList
-          },
-        ],
-        chart: {
-          height: 350,
-          type: "line",
-          dropShadow: {
-            enabled: true,
-            color: "#000",
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 0.2
-          },
-          toolbar: {
-            show: false
-          }
-        },
-        colors: ["#77B6EA", "#545454"],
-        // dataLabels: {
-        //   enabled: true
-        // },
-        stroke: {
-          curve: "smooth"
-        },
-        title: {
-          text: "Call OI VS Put OI",
-          align: "left"
-        },
-        grid: {
-          borderColor: "#e7e7e7",
-          row: {
-            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-            opacity: 0.5
-          }
-        },
-        markers: {
-          size: 1
-        },
-        xaxis: {
-          categories: response.data.getOptionChainDataForStrikePrice.callTimeList
-        },
-        yaxis: {
-        },
-        legend: {
-          position: "top",
-          horizontalAlign: "right",
-          floating: true,
-          offsetY: -25,
-          offsetX: -5
-        }
-      };
+      // this.strikePriceOpenInterestChangeLineChart = {
+      //   series: [
+      //     {
+      //       name: "OI Change",
+      //       data: response.data.getOptionChainDataForStrikePrice.overallChangeInOIList
+      //     },
+      //   ],
+      //   chart: {
+      //     height: 350,
+      //     type: "line",
+      //     dropShadow: {
+      //       enabled: true,
+      //       color: "#000",
+      //       top: 18,
+      //       left: 7,
+      //       blur: 10,
+      //       opacity: 0.2
+      //     },
+      //     toolbar: {
+      //       show: false
+      //     }
+      //   },
+      //   colors: ["#77B6EA", "#545454"],
+      //   // dataLabels: {
+      //   //   enabled: true
+      //   // },
+      //   stroke: {
+      //     curve: "smooth"
+      //   },
+      //   title: {
+      //     text: "Call OI VS Put OI",
+      //     align: "left"
+      //   },
+      //   grid: {
+      //     borderColor: "#e7e7e7",
+      //     row: {
+      //       colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      //       opacity: 0.5
+      //     }
+      //   },
+      //   markers: {
+      //     size: 1
+      //   },
+      //   xaxis: {
+      //     categories: response.data.getOptionChainDataForStrikePrice.callTimeList
+      //   },
+      //   yaxis: {
+      //   },
+      //   legend: {
+      //     position: "top",
+      //     horizontalAlign: "right",
+      //     floating: true,
+      //     offsetY: -25,
+      //     offsetX: -5
+      //   }
+      // };
     }, (err) => console.error(err))
   }
   sum(array: any[]) {
@@ -553,13 +557,12 @@ export class OptionChainAnalysisComponent implements OnInit {
     }
     return totalSum;
   }
-  openStrikePriceSelectionDialog()
-  {
-    this.dialog.open(StrikePriceSelectionComponent,{
+  openStrikePriceSelectionDialog() {
+    this.dialog.open(StrikePriceSelectionComponent, {
       minWidth: 1250,
-      minHeight:600,
-      data:{
-        selectedIndex:this.selectedIndex
+      minHeight: 600,
+      data: {
+        selectedIndex: this.selectedIndex
       }
     })
   }
